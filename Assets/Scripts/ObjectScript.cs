d0e9cbaf5a7abb802210a134056f1a08c1917afe
Jackahashi <jack@startvr.co>
2018-04-07 11:32:37 +1000

    object table, hammers

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour {
+
+
+    public FloorScript floor;
+
+    public AudioClip sfxMusic;
+    public AudioClip sfxFloor;
+    public AudioClip sfxHammer;
+
+    AudioSource audioSource;
+
+    private void Start()
+    {
+         audioSource = GetComponent<AudioSource>();
+    }
+
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hammer")
+        {
+            Debug.Log("the hammer hit the object");
+        }
+        else if (collision.gameObject.tag == "Floor")
+        {
+            Impact();
+            audioSource.PlayOneShot(sfxMusic);
+        }
+            
+    }
+
+
+    public void Impact()
+    {
+        Debug.Log("The object has hit something");
+        //trigger particle effect for 'hit'
+        //trigger particle effect for 'damage points'
+        //make a 'hit' noise 
+        //score the damage points
+        
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RoomTrasherDemo
 Demo of my Udacity Final Project
+
+
+Asset sources:
+
+
+potential music track: Stealing The Car by Tagirijus  non commercial $ licence, so fine if game is free, must be named in credits.
